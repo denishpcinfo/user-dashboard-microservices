b9ad7cebcad43627e619bb34485fcfc1a290372f
Denis Henrique Pacheco da Costa <denishpcinfo@gmail.com>
2025-04-28 13:49:48 -0300

    add credit-card

---
+++ user-info-service/user-info-service/src/main/java/com/d3n15/credit-card-service/credit-card-service/src/test/java/com/d3n15/credit_card_service/CreditCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d3n15.credit_card_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
