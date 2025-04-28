51699228fc4c711671ea2f167c72aedc6fe94a51
Denis Henrique Pacheco da Costa <denishpcinfo@gmail.com>
2025-04-28 13:34:43 -0300

    add  dto-user

---
+++ user-info-service/user-info-service/src/main/java/com/d3n15/user_info_service/dto/UserInfoDTO.java
@@ -0,0 +1,43 @@
+package com.d3n15.user_info_service.dto;
+
+import java.io.Serializable;
+
+public class UserInfoDTO implements Serializable {
+
+    private String name;
+    private String cpf;
+    private String phone;
+
+    public UserInfoDTO() {
+    }
+
+    public UserInfoDTO(String name, String cpf, String phone) {
+        this.name = name;
+        this.cpf = cpf;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
