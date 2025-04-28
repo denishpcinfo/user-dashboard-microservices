937c855df19d950b7faeb863f82b457058c5615f
Denis Henrique Pacheco da Costa <denishpcinfo@gmail.com>
2025-04-28 13:44:23 -0300

    add dto address

---
+++ user-info-service/user-info-service/src/main/java/com/d3n15/address-service/address-service/src/main/java/com/d3n15/address_service/dto/AddressDTO.java
@@ -0,0 +1,63 @@
+package com.d3n15.address_service.dto;
+
+import java.io.Serializable;
+
+public class AddressDTO implements Serializable {
+
+    private String street;
+    private String number;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public AddressDTO() {
+    }
+
+    public AddressDTO(String street, String number, String city, String state, String zipCode) {
+        this.street = street;
+        this.number = number;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
