49a878ab0120bb1369adb5c398f2645ee9e243cb
Denis Henrique Pacheco da Costa <denishpcinfo@gmail.com>
2025-04-28 13:27:41 -0300

    criacao microservico user

---
+++ user-info-service/user-info-service/src/test/java/com/d3n15/user_info_service/UserInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d3n15.user_info_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
